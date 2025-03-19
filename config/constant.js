16dae2820b6482c38926338c87a726cb9c4dee72
gyaneswarpritam <gyaneswar.pritam@gmail.com>
2025-03-20 00:22:10 +0530

    changes

--- config/constant.js
+++ config/constant.js
@@ -1,8 +1,8 @@
 //export const BASE_URL = "http://localhost:5001/api/";
 //export const SOCKET_HOST = "http://localhost:5001";
- export const BASE_URL = "https://dev.neofairs.com/api/";
- export const SOCKET_HOST = "https://dev.neofairs.com:5001";
+export const BASE_URL = "https://bonhoeffer.neofairs.com/api/";
+export const SOCKET_HOST = "https://bonhoeffer.neofairs.com/:5001";
 
 
-export const BUCKET_URL = "https://dev.neofairs.com/images/";
+export const BUCKET_URL = "https://bonhoeffer.neofairs.com/images/";
 
